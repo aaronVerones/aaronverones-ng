ccfc400b0f6545e8ad32ca149d10ec4bc603fd16
Aaron Verones <aaron.verones@alumni.ubc.ca>
2018-07-26 12:11:47 -0700

    update LEMP stack to MEAN stack

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ git push
 # build
 npm run build
 # push to aws
-aws s3 cp ./dist/aaronverones-ng s3://aaronverones --recursive --acl public-read
+aws --profile aaron s3 cp ./dist/aaronverones-ng s3://aaronverones --recursive --acl public-read
